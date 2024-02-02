85e64de639cc17a29e5c41678fd3672a7d14faca
sarahshaoss <sarahs389@nycstudents.net>
2024-02-02 12:26:33 -0500

    s

---
+++ data.py
@@ -0,0 +1,60 @@
+#Data types
+#numbers 1,2,3 etc
+""" def add(x,y):
+    print(x + y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name)
+#1 and "1" are not the same
+add("1","2")
+#undefined/null
+
+#booleans soemthing is true or flase
+tenure = True
+def is_tenured(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("They are not tenured")
+is_tenured(tenure)
+
+values = [1,2,23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+
+day_of_the_week = input("what day is it?")
+if day_of_the_week == "Friday" :
+    print("Correct")
+else:
+    print("Incorrect")
+ """
+temp=75
+if temp > 68:
+    print("warm")
+elif temp ==68:
+    print("perfect")
+else:
+    print("cold")
+
+number = 2
+if number == [1,3,5,7,9]:
+    print("odd")
+else:
+    print("even")
+
+tip = 15
+if tip == 0:
+    print("bad")
+elif tip == 15:
+    print("okay")
+elif tip == 20:
+    print("good")
+elif tip == 25:
+    print("great")
