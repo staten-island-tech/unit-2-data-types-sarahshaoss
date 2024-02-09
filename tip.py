6c8928631ba961997775ca1695e625c693ef8c33
sarahshaoss <sarahs389@nycstudents.net>
2024-02-09 12:28:56 -0500

    w

--- tip.py
+++
@@ -1,9 +0,0 @@
-service = input("how was the service?")
-if service == "bad":
-    print("0% tip")
-elif service == "okay":
-    print("15% tip")
-elif service == "good":
-    print("20% tip")
-elif service == "great":
-    print("25% tip")
