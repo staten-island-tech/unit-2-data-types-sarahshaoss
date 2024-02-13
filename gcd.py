bf545f2e5f466188bb363d166be929f294324389
sarahshaoss <sarahs389@nycstudents.net>
2024-02-13 15:44:07 -0500

    gdsf

--- gcd.py
+++ gcd.py
@@ -1,8 +1,8 @@
-number_1 = int(input("give me a number: "))
-number_2 = int(input("give me another number: "))
+number_1 = int(input("give me a number "))
+number_2 = int(input("give me another number "))
 for i in range (1, number_1 + 1):
     if number_1%i == 0 and number_2%i == 0:
         factors = [i]
 else:
     factors.append
-print(factors) 
+print(factors[-1]) 
